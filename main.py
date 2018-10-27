47f5394930a7ba138c96d5b800d46fb026455165
ethan sorrell <esorrell@crimson.ua.edu>
2018-10-27 18:53:37 -0400

    forgot to clean up before 8^(

--- main.py
+++ main.py
@@ -25,19 +25,12 @@ def next_generation(world):
     return counts
 
 def neighbors(world, cell):
-    "return list of positions of all possible neighbors of cell"
+    "return list of positions of all possible neighbors"
     xs = [cell[0]+i for i in [-1, 0, 1]]
     ys = [cell[1]+i for i in [-1, 0, 1]]
     possible_neighbors = ((x,y) for x in xs for y in ys
                           if 0 <= x < 50 and 0 <= y < 50)
     return possible_neighbors
-
-def live(world):
-    "returns a series of livelihood of cells in world"
-    livelihood = dict()
-    for index in world.index:
-        livelihood[index] = 0 if world.ix[index]['health'] < threshold else 1
-    return pd.Series(livelihood)
 
 def display(world):
     "display world as plot with points at locations of living cells"
@@ -58,7 +51,6 @@ def updater(i):
     colors = [v[1:]+(1,) for k,v in generations[i].items() if not (v[0] < threshold)]
     scat.set_offsets(living)
     scat.set_color(colors)
-
 
 fig, ax = plt.subplots(figsize=(10,10))
 ax.set_xlim(-1,51)
@@ -88,48 +80,9 @@ living = [k for k,v in world.items() if not (v[0] < threshold)]
 scat = ax.scatter([x for x in map(lambda a: a[0], living)],
                   [y for y in map(lambda a: a[1], living)])
 
-print(scat.get_edgecolor())
 generations = [world]
 for i in range(num_frames):
     generations.append(next_generation(generations[i]))
 
 ani = anim.FuncAnimation(fig, updater, interval=200, frames=range(num_frames))
 plt.show()
-
-# fig, ax = plt.subplots(figsize=(10,10))
-# ax.set_xlim(-1,51)
-# ax.set_ylim(-1,51)
-# ax.set_xticks(np.arange(-0.5,x_dim+0.5))
-# ax.set_yticks(np.arange(-0.5,y_dim+0.5))
-# ax.set_xticklabels([])
-# ax.set_yticklabels([])
-# ax.grid(True)
-# 
-# world = {key:(0, 0, 0, 0) for key in ((x,y) for x in range(x_dim) for y in range(y_dim))}
-# 
-# world[(5,5)] = (1, 0, 0, 255)
-# world[(6,5)] = (1, 0, 0, 255)
-# world[(7,5)] = (1, 0, 0, 255)
-# world[(11,8)] = (1, 0, 255, 0)
-# world[(11,9)] = (1, 0, 255, 0)
-# world[(21,20)] = (1, 255, 0, 0)
-# world[(21,22)] = (1, 255, 0, 0)
-# world[(41,40)] = (1, 255, 0, 0)
-# world[(41,42)] = (1, 255, 0, 0)
-# world[(42,43)] = (1, 255, 0, 0)
-# world[(42,44)] = (1, 255, 0, 0)
-# 
-# 
-# num_frames = 25
-# living = [k for k,v in world.items() if not (v[0] < threshold)]
-# 
-# scat = ax.scatter([x for x in map(lambda a: a[0], living)],
-#                   [y for y in map(lambda a: a[1], living)])
-# 
-# generations = [world]
-# for i in range(num_frames):
-#     generations.append(next_generation(generations[i]))
-# 
-# 
-# ani = anim.FuncAnimation(fig, updater, interval=200, frames=range(num_frames))
-# plt.show()
